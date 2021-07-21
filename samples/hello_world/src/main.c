90a986dc88f6d98e48bf2a6294e1759a6b2839ee
Simon <simon.iver93@gmail.com>
2021-07-21 13:33:45 +0200

    Could get spi cs flag and pin using dts

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,8 +6,13 @@
 
 #include <zephyr.h>
 #include <sys/printk.h>
+#include <devicetree.h>
+
+#define SPI2 DT_NODELABEL(spi2)
 
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+	printk("CS GPIOS %d\n", DT_PHA_BY_IDX(SPI2, cs_gpios, 0, pin));
+	printk("CS FLAGS %d\n", DT_PHA_BY_IDX(SPI2, cs_gpios, 0, flags));
 }
