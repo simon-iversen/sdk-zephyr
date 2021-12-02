8e6b40a02cf67f6df889491f5ccd6c28019c1495
Simon <simon.iver93@gmail.com>
2021-12-02 15:14:11 +0100

    Added serial DFU support to hello world

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,8 +6,13 @@
 
 #include <zephyr.h>
 #include <sys/printk.h>
+#include "os_mgmt/os_mgmt.h"
+#include "img_mgmt/img_mgmt.h"
 
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+	printk("build time: " __DATE__ " " __TIME__ "\n");
+	os_mgmt_register_group();
+	img_mgmt_register_group();
 }
